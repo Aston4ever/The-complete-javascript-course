fa9f1b79a1ab6b8b07b582d1b1438a3f0695a3c5
aston4ever <aston4ever@gmail.com>
2022-12-23 01:37:10 +0300

    feat: add strict mode

---
+++ 03_JavaScript_Fundamentals_Part_2/script.js
@@ -0,0 +1,12 @@
+'use strict';
+// <------------------------------Strict Mode
+/*
+'use strict';
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) haseDriverLicense = true; //instead of use previous var it creates new global variable if not in strict mode
+if (hasDriverLicense) console.log("I can drive ^^");*/
+
+// <------------------------------Functions
